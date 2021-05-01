59c871513c6d95f7c57b695dbcdc60b5d867102f
Evan <evanfujita@gmail.com>
2021-05-01 13:12:42 -0500

    hard restart

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import './App.css';
-import LoginForm from './components/LoginForm'
-import Register from './components/Register'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import UserHome from './components/UserHome'
-import { Switch, Route } from 'react-router-dom'
-import { Grid } from 'semantic-ui-react'
 
 function App() {
   return (
-    <Grid>
-      <Grid.Row>
-        Welcome
-      </Grid.Row>
-      <Grid.Row>
-      <Grid.Column>
-          <Navbar />
-        </Grid.Column>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={LoginForm}/>
-        <Route exact path='/register' component={Register}/>
-        <Route exact path='/profile' component={UserHome}/>
-      </Switch>
-      </Grid.Row>
-    </Grid>
+
+        <p>Welcome</p>
+
   );
 }
 
