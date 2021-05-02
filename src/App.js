5d3b3c477965ddd7c8e73e7dc4f1775855daa269
Evan <evanfujita@gmail.com>
2021-05-02 10:16:59 -0500

    login form component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
-
+        <div>
         <p>Welcome</p>
-
+        <Login />
+        </div>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import LoginForm from './forms/LoginForm'
 
 class Login extends React.Component{
     render(){
         return(
             <div>
-
+                <LoginForm />
             </div>
         )
     }
