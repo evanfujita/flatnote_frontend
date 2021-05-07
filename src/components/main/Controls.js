131b23d2562ba6d9578cd8d9247985b44e2e2b30
Evan <evanfujita@gmail.com>
2021-05-07 17:56:31 -0500

    added controls for edit / delete in navbar

---
+++ src/components/main/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Controls = () => {
+    return (
+        <span class='controls'>
+            <li class='navbar-item'>edit</li>
+            <li class='navbar-item'>delete</li>
+        </span>
+    )
+}
+
+export default Controls
--- src/components/navbar/LoggedIn.js
+++ src/components/navbar/LoggedIn.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import Controls from '../main/Controls'
+import { useSelector, useDispatch } from 'react-redux'
 import { logout, addNoteForm } from '../../actions/index'
 
 const LoggedIn = () => {
-    
+    const selectedNote = useSelector(state => state.selections.note)
     const dispatch = useDispatch()
 
     return (
         <ul>
             <li class='navbar-item' id='Notes'  >notes</li>
             <li class='navbar-item' id='addNote' onClick={()=> dispatch(addNoteForm())} >add note</li>
+            {selectedNote ? <Controls /> : null}
             <span class='positioned'>
                 <li class='navbar-item' onClick={()=> dispatch(logout())}>logout</li>
             </span>
