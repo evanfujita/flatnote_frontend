7919015bb1b3de826b774b93cd08faa0e4ab9b51
Evan <evanfujita@gmail.com>
2021-05-10 18:07:32 -0500

    removed state hook from notes container

--- src/components/main/NotesContainer.js
+++ src/components/main/NotesContainer.js
@@ -1,34 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import DisplayItem from './DisplayItem'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectNote } from '../../actions/index'
+import { useSelector } from 'react-redux'
+
 
 const NotesContainer = () => {
     const notes = useSelector(state => state.notes)
     const tasks = useSelector(state => state.tasks)
     const viewNotes = useSelector(state => state.selections.viewNotes)
     const viewTasks = useSelector(state => state.selections.viewTasks)
-    const dispatch = useDispatch()
-    // const [selectTitle, setSelector] = useState(0)
-    
-    const handleClick = note => {
-        dispatch(selectNote(note))
-        // setSelector(note.id)
-    }
 
     const displayNotes = 
         viewNotes 
         ?
-        notes.map(note => <DisplayItem handleClick={handleClick} item={note} type='note' />)
-        // <p key={note.id} id={note.id} class={selectTitle == note.id ? 'selected' : 'title'} onClick={()=> handleClick(note)}>{note.title}</p>)
+        notes.map(note => <DisplayItem item={note} type='note' />)
         :
         null
 
     const displayTasks =
         viewTasks
         ?
-        tasks.map(task => <DisplayItem handleClick={handleClick} item={task} type='task' />)
-        // <p key={task.id} id={task.id} class={selectTitle == task.id ? 'selected' : 'title'} onClick={()=> handleClick(task)}>{task.title}</p>)
+        tasks.map(task => <DisplayItem item={task} type='task' />)
         :
         null
 
