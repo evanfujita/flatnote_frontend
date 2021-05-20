4b583ffbd0979e9fa6dd13afc8b6786c9849691d
Evan <evanfujita@gmail.com>
2021-05-20 00:39:04 -0500

    fixed Login

--- src/components/main/Login.js
+++ src/components/main/Login.js
@@ -6,14 +6,36 @@ import { loginFetch, postFetch, createReqObj } from '../../helpers/fetch'
 import { loginAuth, loginFail } from '../../actions/user'
 
 const Login = props => {
-    const [state, setState] = useState('')
+    const { type } = props
+    const [state, setState] = useState({})
     
     const dispatch = useDispatch()
     
-    const { type } = props
     const handleChange = event => {
         setState({
-            [event.target.name]:event.target.value
+            ...state,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const loginFetch = (reqObj) => {
+        // debugger
+        fetch('http://localhost:3000/login', reqObj)
+        .then(resp => resp.json())
+        .then(user => {
+            // debugger
+            if (user.id){dispatch(loginAuth(user))}
+            if (user.error){loginFail()}
+        })
+    }
+    
+    const postFetch = (resource, reqObj, reducer) => {
+        fetch(`http://localhost:3000/${resource}`, reqObj)
+        .then(resp => resp.json())
+        .then(data => {
+            if(reducer){
+                reducer(data)
+            }
         })
     }
 
