ce7e1a972aabc3801e9583348519d39e532b6246
Evan <evanfujita@gmail.com>
2021-06-04 10:20:53 -0500

    thunk installed, imported, applied

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App';
+import thunkMiddleware from 'redux-thunk'
 import reportWebVitals from './reportWebVitals';
 import reducer from './reducers/reducers'
 import 'semantic-ui-css/semantic.min.css'
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 ReactDOM.render(
     <Provider store={store}>
